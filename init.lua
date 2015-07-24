653ba26079b0a2b59a50c169711cdf8a7b61fccf
yamanq <ybq987@gmail.com>
2015-07-23 22:43:00 -0400

    added inheritance chance

--- init.lua
+++ init.lua
@@ -251,9 +251,9 @@ function ager(player, age)
 
     -- Chances
     local hotbarChance = 20 / (1 + 100 * math.pow(e, -0.09 * age))
-    local cancerChance = 10 / (1 + 40 * math.pow(e,-0.05 * age))
-    local slowMoveChance = 15 / (1 + 500 * math.pow(e,-0.1 * age))
-    local inheritChance = 15 / (1 + 500 * math.pow(e,-0.1 * age))
+    local cancerChance = 10 / (1 + 40 * math.pow(e, -0.05 * age))
+    local slowMoveChance = 15 / (1 + 500 * math.pow(e, -0.1 * age))
+    local inheritChance = 25 / (1 + math.pow(e, -0.16 * (age - 58))
 
     -- Hotbar Decreaser
     if math.random(100) <= hotbarChance and not aging.agingPeople[playerName].hotbarEnabled then
